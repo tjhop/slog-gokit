6b75987fc0ee781e0b4b4e7998f20afb253cad2f
TJ Hoplock <t.hoplock@gmail.com>
2024-11-19 15:24:07 -0500

    feat: init custom slog Handler to wrap go-kit Logger
    Signed-off-by: TJ Hoplock <t.hoplock@gmail.com>
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

---
+++ handler_test.go
@@ -0,0 +1,142 @@
+package sloggokit
+
+import (
+	"bytes"
+	"fmt"
+	"log/slog"
+	"regexp"
+	"strings"
+	"testing"
+	"testing/slogtest"
+
+	"github.com/go-kit/log"
+	"github.com/go-logfmt/logfmt"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	logRegexp = regexp.MustCompile(`level=(?P<LevelValue>warn|info|error|debug).*time=.+msg=.+`)
+)
+
+func TestNewGoKitHandler(t *testing.T) {
+	t.Run("nil level", func(t *testing.T) {
+		var buf bytes.Buffer
+		h := NewGoKitHandler(log.NewLogfmtLogger(&buf), nil)
+
+		results := func() []map[string]any {
+			var ms []map[string]any
+
+			// Print logs for humans.
+			fmt.Println(buf.String())
+
+			dec := logfmt.NewDecoder(&buf)
+			for dec.ScanRecord() {
+				m := make(map[string]any)
+
+				for dec.ScanKeyval() {
+					m[string(dec.Key())] = dec.Value()
+				}
+				ms = append(ms, m)
+			}
+			if err := dec.Err(); err != nil {
+				t.Fatal(err)
+			}
+
+			return ms
+		}
+
+		err := slogtest.TestHandler(h, results)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+	t.Run("debug level", func(t *testing.T) {
+		var buf bytes.Buffer
+		lvl := &slog.LevelVar{}
+		lvl.Set(slog.LevelDebug)
+		h := NewGoKitHandler(log.NewLogfmtLogger(&buf), lvl)
+
+		results := func() []map[string]any {
+			var ms []map[string]any
+
+			// Print logs for humans.
+			fmt.Println(buf.String())
+
+			dec := logfmt.NewDecoder(&buf)
+			for dec.ScanRecord() {
+				m := make(map[string]any)
+
+				for dec.ScanKeyval() {
+					m[string(dec.Key())] = dec.Value()
+				}
+				ms = append(ms, m)
+			}
+			if err := dec.Err(); err != nil {
+				t.Fatal(err)
+			}
+
+			return ms
+		}
+
+		err := slogtest.TestHandler(h, results)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+	t.Run("dynamic level", func(t *testing.T) {
+		var buf bytes.Buffer
+		lvl := &slog.LevelVar{}
+
+		gklogger := log.NewLogfmtLogger(&buf)
+		h := NewGoKitHandler(gklogger, lvl)
+		slogger := slog.New(h)
+
+		wantedLevelCounts := map[string]int{"info": 1, "debug": 1}
+
+		// Start at debug level.
+		lvl.Set(slog.LevelDebug)
+		slogger.Info("info", "hello", "world")
+		slogger.Debug("debug", "hello", "world")
+
+		// We expect to see one of each log level type in `wantedLevelCounts`
+		counts := getLogEntryLevelCounts(buf.String(), logRegexp)
+		require.Equal(t, wantedLevelCounts["info"], counts["info"], "info log successfully detected")
+		require.Equal(t, wantedLevelCounts["debug"], counts["debug"], "debug log successfully detected")
+
+		// Print logs for humans.
+		fmt.Println(buf.String())
+		buf.Reset()
+
+		// Test that log level can be adjusted on-the-fly to info and
+		// that a subsequent call to write a debug level log is _not_
+		// written to the file.
+		lvl.Set(slog.LevelInfo)
+
+		slogger.Info("info", "hello", "world")
+		slogger.Debug("debug", "hello", "world")
+
+		// We expect to see one info log, and 0 debug logs.
+		counts = getLogEntryLevelCounts(buf.String(), logRegexp)
+		require.Equal(t, wantedLevelCounts["info"], counts["info"], "info log successfully detected")
+		require.NotEqual(t, wantedLevelCounts["debug"], counts["debug"], "extra debug log detected")
+
+		// Print logs for humans to see, if needed.
+		fmt.Println(buf.String())
+		buf.Reset()
+	})
+}
+
+func getLogEntryLevelCounts(s string, re *regexp.Regexp) map[string]int {
+	counters := make(map[string]int)
+	lines := strings.Split(s, "\n")
+	for _, line := range lines {
+		matches := re.FindStringSubmatch(line)
+		if len(matches) > 1 {
+			levelIndex := re.SubexpIndex("LevelValue")
+
+			counters[strings.ToLower(matches[levelIndex])]++
+		}
+	}
+
+	return counters
+}
